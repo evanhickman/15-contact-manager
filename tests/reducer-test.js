7083368f840fd724327d0497eb91d2fd2f2ff0bb
Evan Hickman <travisevanhickman@gmail.com>
2017-02-27 16:50:16 -0600

    pass final test

--- tests/reducer-test.js
+++ tests/reducer-test.js
@@ -43,11 +43,17 @@ module('reducer', () => {
 
     assert.deepEqual(reducer(oldState, action), expectedState);
   });
-
   test('it can remove a contact based on id while leaving others', (assert) => {
     const oldState = { contacts: [{ firstName: 'Paul', lastName: 'Blart', id: 1 }, { firstName: 'Paul', lastName: 'Blart', id: 2 }] };
     const action = { type: 'CONTACT@REMOVE', id: 1  };
     const expectedState = { contacts: [{ firstName: 'Paul', lastName: 'Blart', id: 2 }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
+  });
+  test('it cannot remove a contact with an id that does not exist', (assert) => {
+    const oldState = { contacts: [{ firstName: 'Paul', lastName: 'Blart', id: 1 }] };
+    const action = { type: 'CONTACT@REMOVE', id: 2 };
+    const expectedState = { contacts: [{ firstName: 'Paul', lastName: 'Blart', id: 1 }] };
 
     assert.deepEqual(reducer(oldState, action), expectedState);
   });
